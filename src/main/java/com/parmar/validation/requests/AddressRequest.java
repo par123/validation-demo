ecc745eb7884024462f4a2affcf0fe97f8abddb1
par123 <dharmparm474@gmail.com>
2024-01-10 19:10:23 +0530

    pushing Address requets changes

--- src/main/java/com/parmar/validation/requests/AddressRequest.java
+++ src/main/java/com/parmar/validation/requests/AddressRequest.java
@@ -1,4 +1,23 @@
 package com.parmar.validation.requests;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddressRequest {
+
+    @NotEmpty
+    private int id;
+    private String country;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String street;
+    @NotNull
+    private int userId;
 }
